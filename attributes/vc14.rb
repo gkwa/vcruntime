d0d04c668f7fa437e541e34a19ea276ce7fc07d9
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-09-03 22:14:12 -0700

    Add Visual C++ 2015 runtimes

---
+++ attributes/vc14.rb
@@ -0,0 +1,27 @@
+#
+# Author:: Julian C. Dunn (<jdunn@chef.io>)
+# Cookbook Name:: vcruntime
+# Attributes:: vc12
+# Copyright 2014-2015, Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+default['vcruntime']['vc14']['x86']['14.0.23026']['url']       = 'https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe'
+default['vcruntime']['vc14']['x86']['14.0.23026']['sha256sum'] = 'fdd1e1f0dcae2d0aa0720895eff33b927d13076e64464bb7c7e5843b7667cd14'
+default['vcruntime']['vc14']['x86']['14.0.23026']['name']      = 'Microsoft Visual C++ 2015 Redistributable (x86) - version'
+default['vcruntime']['vc14']['x64']['14.0.23026']['url']       = 'https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe'
+default['vcruntime']['vc14']['x64']['14.0.23026']['sha256sum'] = '5eea714e1f22f1875c1cb7b1738b0c0b1f02aec5ecb95f0fdb1c5171c6cd93a3'
+default['vcruntime']['vc14']['x64']['14.0.23026']['name']      = 'Microsoft Visual C++ 2015 Redistributable (x64) - version'
+
+default['vcruntime']['vc14']['version'] = '14.0.23026'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yvo@chef.io'
 license          'Apache 2.0'
 description      'Installs Microsoft Visual C++ Runtimes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 %w( windows ).each do |os|
   supports os
@@ -15,10 +15,12 @@ depends 'windows'
 provides 'vcruntime::vc9'
 provides 'vcruntime::vc10'
 provides 'vcruntime::vc12'
+provides 'vcruntime::vc14'
 
 recipe	'vcruntime::vc9', 'Visual C++ 2008 runtimes'
 recipe	'vcruntime::vc10', 'Visual C++ 2010 runtimes'
 recipe	'vcruntime::vc12', 'Visual C++ 2012 runtimes'
+recipe	'vcruntime::vc14', 'Visual C++ 2015 runtimes'
 
 source_url 'https://github.com/chef-windows/vcruntime' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-windows/vcruntime/issues' if respond_to?(:issues_url)
