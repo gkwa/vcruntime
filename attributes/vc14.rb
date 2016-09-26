ac40fd3f17d6d02193e614d4ea20fa90d9e8cf68
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-09-25 18:16:15 -0700

    node.default to just default

--- attributes/vc14.rb
+++ attributes/vc14.rb
@@ -23,70 +23,70 @@ if node['platform_family'] == 'windows'
   if win_version.windows_vista?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/D/8/3/D838D576-232C-4C17-A402-75913F27113B/Windows6.0-KB2999226-x86.msu'
-      node.default['KB2999226']['checksum'] = 'ae380f63bf4e8700ada686406b04b01230a339b09edf7819814a4c0bf4ab72e1'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/D/8/3/D838D576-232C-4C17-A402-75913F27113B/Windows6.0-KB2999226-x86.msu'
+      default['KB2999226']['checksum'] = 'ae380f63bf4e8700ada686406b04b01230a339b09edf7819814a4c0bf4ab72e1'
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/5/4/E/54E27BE2-CFB2-4FC9-AB03-C39302CA68A0/Windows6.0-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '10069de7315ca3f405e2579846af5dab3089a8496ae4c1ab61763480f43a05a8'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/5/4/E/54E27BE2-CFB2-4FC9-AB03-C39302CA68A0/Windows6.0-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '10069de7315ca3f405e2579846af5dab3089a8496ae4c1ab61763480f43a05a8'
     end
 
   elsif win_version.windows_7?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/4/F/E/4FE73868-5EDD-4B47-8B33-CE1BB7B2B16A/Windows6.1-KB2999226-x86.msu'
-      node.default['KB2999226']['checksum'] = '909e76c81ef0eb176144b253ddffe7a8fdfacebfaa15e97def003d2262fbf084'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/4/F/E/4FE73868-5EDD-4B47-8B33-CE1BB7B2B16A/Windows6.1-KB2999226-x86.msu'
+      default['KB2999226']['checksum'] = '909e76c81ef0eb176144b253ddffe7a8fdfacebfaa15e97def003d2262fbf084'
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/1/1/5/11565A9A-EA09-4F0A-A57E-520D5D138140/Windows6.1-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/1/1/5/11565A9A-EA09-4F0A-A57E-520D5D138140/Windows6.1-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
     end
 
   elsif win_version.windows_8?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/1/E/8/1E8AFE90-5217-464D-9292-7D0B95A56CE4/Windows8-RT-KB2999226-x86.msu'
-      node.default['KB2999226']['checksum'] = '0f36750fbb06fee23131f68b4d0943841eed24730ec1d5d77dedc41d359be88d'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/1/E/8/1E8AFE90-5217-464D-9292-7D0B95A56CE4/Windows8-RT-KB2999226-x86.msu'
+      default['KB2999226']['checksum'] = '0f36750fbb06fee23131f68b4d0943841eed24730ec1d5d77dedc41d359be88d'
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/A/C/1/AC15393F-A6E6-469B-B222-C44B3BB6ECCC/Windows8-RT-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/A/C/1/AC15393F-A6E6-469B-B222-C44B3BB6ECCC/Windows8-RT-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
     end
 
   elsif win_version.windows_8_1?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/E/4/6/E4694323-8290-4A08-82DB-81F2EB9452C2/Windows8.1-KB2999226-x86.msu'
-      node.default['KB2999226']['checksum'] = 'b83251219c5390536b02bebaf5e43a6f13381ce1db43e76483bce07c4bcf877b'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/E/4/6/E4694323-8290-4A08-82DB-81F2EB9452C2/Windows8.1-KB2999226-x86.msu'
+      default['KB2999226']['checksum'] = 'b83251219c5390536b02bebaf5e43a6f13381ce1db43e76483bce07c4bcf877b'
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/6/F/96FD0525-3DDF-423D-8845-5F92F4A6883E/Windows8.1-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '9f707096c7d279ed4bc2a40ba695efac69c20406e0ca97e2b3e08443c6381d15'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/6/F/96FD0525-3DDF-423D-8845-5F92F4A6883E/Windows8.1-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '9f707096c7d279ed4bc2a40ba695efac69c20406e0ca97e2b3e08443c6381d15'
     end
 
   elsif win_version.windows_server_2012_r2?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = ''
-      node.default['KB2999226']['checksum'] = ''
+      default['KB2999226']['url'] = ''
+      default['KB2999226']['checksum'] = ''
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/F/1/3/F13BEC9A-8FC6-4489-9D6A-F84BDC9496FE/Windows6.1-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/F/1/3/F13BEC9A-8FC6-4489-9D6A-F84BDC9496FE/Windows6.1-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
     end
   elsif win_version.windows_server_2012?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = ''
-      node.default['KB2999226']['checksum'] = ''
+      default['KB2999226']['url'] = ''
+      default['KB2999226']['checksum'] = ''
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/3/E/93E0745A-EAE9-4B5A-B50C-012F2D3B6659/Windows8-RT-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/3/E/93E0745A-EAE9-4B5A-B50C-012F2D3B6659/Windows8-RT-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
     end
 
   elsif win_version.windows_server_2008? || win_version.windows_server_2008_r2?
     case node['kernel']['machine']
     when /i[3-6]86/
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/B/5/7/B5757251-DAB0-4E23-AA46-ABC233FDB90E/Windows6.0-KB2999226-x86.msu'
-      node.default['KB2999226']['checksum'] = 'ae380f63bf4e8700ada686406b04b01230a339b09edf7819814a4c0bf4ab72e1'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/B/5/7/B5757251-DAB0-4E23-AA46-ABC233FDB90E/Windows6.0-KB2999226-x86.msu'
+      default['KB2999226']['checksum'] = 'ae380f63bf4e8700ada686406b04b01230a339b09edf7819814a4c0bf4ab72e1'
     when 'x86_64'
-      node.default['KB2999226']['url'] = 'https://download.microsoft.com/download/A/7/A/A7A70B17-ADF9-4FC3-A722-69FA89B79756/Windows6.0-KB2999226-x64.msu'
-      node.default['KB2999226']['checksum'] = '10069de7315ca3f405e2579846af5dab3089a8496ae4c1ab61763480f43a05a8'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/A/7/A/A7A70B17-ADF9-4FC3-A722-69FA89B79756/Windows6.0-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '10069de7315ca3f405e2579846af5dab3089a8496ae4c1ab61763480f43a05a8'
     end
   end
 end
