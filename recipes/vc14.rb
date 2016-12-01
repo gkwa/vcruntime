f36b22e0e507a6fac664f1ff873526d6f6dc1670
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-11-30 22:24:11 -0800

    Work around for Download...SSL/TLS secure channel error
    Without this, I'm getting this error when trying to download.
    
    Exception calling "DownloadFile" with "2" argument(s): "The underlying
    connection was closed: Could not establish trust relationship for the
    SSL/TLS secure channel."

--- recipes/vc14.rb
+++ recipes/vc14.rb
@@ -54,6 +54,47 @@ def install_dependency
   end
 end
 
+powershell_script 'Disable automatic proxy detection' do
+  code <<-EOH
+function Disable-AutomaticallyDetectProxySettings
+{
+    # Read connection settings from Internet Explorer.
+    $regKeyPath = "HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections\\"
+    $conSet = $(Get-ItemProperty $regKeyPath).DefaultConnectionSettings
+ 
+    # Index into DefaultConnectionSettings where the relevant flag resides.
+    $flagIndex = 8
+ 
+    # Bit inside the relevant flag which indicates whether or not to enable automatically detect proxy settings.
+    $autoProxyFlag = 8
+ 
+    if ($($conSet[$flagIndex] -band $autoProxyFlag) -eq $autoProxyFlag)
+    {
+        # 'Automatically detect proxy settings' was enabled, adding one disables it.
+        Write-Host "Disabling 'Automatically detect proxy settings'."
+        $mask = -bnot $autoProxyFlag
+        $conSet[$flagIndex] = $conSet[$flagIndex] -band $mask
+        $conSet[4]++
+        Set-ItemProperty -Path $regKeyPath -Name DefaultConnectionSettings -Value $conSet
+    }
+    
+    $conSet = $(Get-ItemProperty $regKeyPath).DefaultConnectionSettings
+    if ($($conSet[$flagIndex] -band $autoProxyFlag) -ne $autoProxyFlag)
+    {
+    	Write-Host "'Automatically detect proxy settings' is disabled."
+    }
+}
+
+$job = Start-Job -ScriptBlock { Start-Process "C:\\Program Files\\Internet Explorer\\iexplore.exe" -Wait -PassThru }
+Start-Sleep -Seconds 5
+Stop-Job -Id $job.Id
+
+Disable-AutomaticallyDetectProxySettings
+
+Set-Itemproperty "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections" -Name ProxyEnable -Value 0
+  EOH
+end
+
 if node.attribute?('KB2999226') and !node['KB2999226']['url'].to_s.empty?
   install_dependency
 end
