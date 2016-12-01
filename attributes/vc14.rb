614efa848b5d781086304526dad34ae16bb0222e
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-01 14:56:10 -0800

    redo urls

--- attributes/vc14.rb
+++ attributes/vc14.rb
@@ -17,20 +17,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+https://download.microsoft.com/download/9/3/E/93E0745A-EAE9-4B5A-B50C-012F2D3B6659/Windows8-RT-KB2999226-x64.msu
+
+
 if node['platform_family'] == 'windows'
   require 'chef/win32/version'
   win_version = Chef::ReservedNames::Win32::Version.new
-  if win_version.windows_vista?
-    case node['kernel']['machine']
-    when /i[3-6]86/
-      default['KB2999226']['url'] = 'https://download.microsoft.com/download/D/8/3/D838D576-232C-4C17-A402-75913F27113B/Windows6.0-KB2999226-x86.msu'
-      default['KB2999226']['checksum'] = 'ae380f63bf4e8700ada686406b04b01230a339b09edf7819814a4c0bf4ab72e1'
-    when 'x86_64'
-      default['KB2999226']['url'] = 'https://download.microsoft.com/download/5/4/E/54E27BE2-CFB2-4FC9-AB03-C39302CA68A0/Windows6.0-KB2999226-x64.msu'
-      default['KB2999226']['checksum'] = '10069de7315ca3f405e2579846af5dab3089a8496ae4c1ab61763480f43a05a8'
-    end
-
-  elsif win_version.windows_7?
+  if win_version.windows_7?
     case node['kernel']['machine']
     when /i[3-6]86/
       default['KB2999226']['url'] = 'https://download.microsoft.com/download/4/F/E/4FE73868-5EDD-4B47-8B33-CE1BB7B2B16A/Windows6.1-KB2999226-x86.msu'
@@ -39,7 +32,15 @@ if node['platform_family'] == 'windows'
       default['KB2999226']['url'] = 'https://download.microsoft.com/download/1/1/5/11565A9A-EA09-4F0A-A57E-520D5D138140/Windows6.1-KB2999226-x64.msu'
       default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
     end
-
+  elsif win_version.windows_2008_r2?
+    case node['kernel']['machine']
+    when /i[3-6]86/
+      default['KB2999226']['url'] = ''
+      default['KB2999226']['checksum'] = ''
+    when 'x86_64'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/F/1/3/F13BEC9A-8FC6-4489-9D6A-F84BDC9496FE/Windows6.1-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
+    end
   elsif win_version.windows_8?
     case node['kernel']['machine']
     when /i[3-6]86/
@@ -49,7 +50,24 @@ if node['platform_family'] == 'windows'
       default['KB2999226']['url'] = 'https://download.microsoft.com/download/A/C/1/AC15393F-A6E6-469B-B222-C44B3BB6ECCC/Windows8-RT-KB2999226-x64.msu'
       default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
     end
-
+  elsif windows_server_2012_r2?
+    case node['kernel']['machine']
+    when /i[3-6]86/
+      default['KB2999226']['url'] = ''
+      default['KB2999226']['checksum'] = ''
+    when 'x86_64'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/D/1/3/D13E3150-3BB2-4B22-9D8A-47EE2D609FFF/Windows8.1-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '9f707096c7d279ed4bc2a40ba695efac69c20406e0ca97e2b3e08443c6381d15'
+    end
+  elsif windows_server_2012?
+    case node['kernel']['machine']
+    when /i[3-6]86/
+      default['KB2999226']['url'] = ''
+      default['KB2999226']['checksum'] = ''
+    when 'x86_64'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/3/E/93E0745A-EAE9-4B5A-B50C-012F2D3B6659/Windows8-RT-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
+    end
   elsif win_version.windows_8_1?
     case node['kernel']['machine']
     when /i[3-6]86/
@@ -59,27 +77,16 @@ if node['platform_family'] == 'windows'
       default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/6/F/96FD0525-3DDF-423D-8845-5F92F4A6883E/Windows8.1-KB2999226-x64.msu'
       default['KB2999226']['checksum'] = '9f707096c7d279ed4bc2a40ba695efac69c20406e0ca97e2b3e08443c6381d15'
     end
-
-  elsif win_version.windows_server_2012_r2?
+  elsif win_version.windows_vista? || win_version.windows_2008?
     case node['kernel']['machine']
     when /i[3-6]86/
-      default['KB2999226']['url'] = ''
-      default['KB2999226']['checksum'] = ''
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/D/8/3/D838D576-232C-4C17-A402-75913F27113B/Windows6.0-KB2999226-x86.msu'
+      default['KB2999226']['checksum'] = 'ae380f63bf4e8700ada686406b04b01230a339b09edf7819814a4c0bf4ab72e1'
     when 'x86_64'
-      default['KB2999226']['url'] = 'https://download.microsoft.com/download/F/1/3/F13BEC9A-8FC6-4489-9D6A-F84BDC9496FE/Windows6.1-KB2999226-x64.msu'
-      default['KB2999226']['checksum'] = '43234d2986ca9b0de75d5183977964d161a8395c3396279ddfc9b20698e5bc34'
+      default['KB2999226']['url'] = 'https://download.microsoft.com/download/5/4/E/54E27BE2-CFB2-4FC9-AB03-C39302CA68A0/Windows6.0-KB2999226-x64.msu'
+      default['KB2999226']['checksum'] = '10069de7315ca3f405e2579846af5dab3089a8496ae4c1ab61763480f43a05a8'
     end
-  elsif win_version.windows_server_2012?
-    case node['kernel']['machine']
-    when /i[3-6]86/
-      default['KB2999226']['url'] = ''
-      default['KB2999226']['checksum'] = ''
-    when 'x86_64'
-      default['KB2999226']['url'] = 'https://download.microsoft.com/download/9/3/E/93E0745A-EAE9-4B5A-B50C-012F2D3B6659/Windows8-RT-KB2999226-x64.msu'
-      default['KB2999226']['checksum'] = '50cae25da33fa950222d1a803e42567291eb7feb087fa119b1c97fe9d41cd9f8'
-    end
-
-  elsif win_version.windows_server_2008? || win_version.windows_server_2008_r2?
+  elsif win_version.windows_vista?
     case node['kernel']['machine']
     when /i[3-6]86/
       default['KB2999226']['url'] = 'https://download.microsoft.com/download/B/5/7/B5757251-DAB0-4E23-AA46-ABC233FDB90E/Windows6.0-KB2999226-x86.msu'
