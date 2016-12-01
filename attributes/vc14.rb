185ca0e6c94b534bc2f98e835e5c8684ba2daebe
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-01 09:37:52 -0800

    PHP won't run without this one

--- attributes/vc14.rb
+++ attributes/vc14.rb
@@ -105,4 +105,11 @@ default['vcruntime']['vc14']['x64']['14.0.24212']['url']       = 'https://downlo
 default['vcruntime']['vc14']['x64']['14.0.24212']['sha256sum'] = 'da66717784c192f1004e856bbcf7b3e13b7bf3ea45932c48e4c9b9a50ca80965'
 default['vcruntime']['vc14']['x64']['14.0.24212']['name']      = 'Microsoft Visual C++ 2015 Redistributable (x64) - version'
 
-default['vcruntime']['vc14']['version'] = '14.0.24212'
+default['vcruntime']['vc14']['x86']['14.0.24215']['url']       = 'https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x86.exe'
+default['vcruntime']['vc14']['x86']['14.0.24215']['sha256sum'] = '12a69af8623d70026690ba14139bf3793cc76c865759cad301b207c1793063ed'
+default['vcruntime']['vc14']['x86']['14.0.24215']['name']      = 'Microsoft Visual C++ 2015 Redistributable (x86) - version'
+default['vcruntime']['vc14']['x64']['14.0.24215']['url']       = 'https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x64.exe'
+default['vcruntime']['vc14']['x64']['14.0.24215']['sha256sum'] = 'da66717784c192f1004e856bbcf7b3e13b7bf3ea45932c48e4c9b9a50ca80965'
+default['vcruntime']['vc14']['x64']['14.0.24215']['name']      = 'Microsoft Visual C++ 2015 Redistributable (x64) - version'
+
+default['vcruntime']['vc14']['version'] = '14.0.24215'
