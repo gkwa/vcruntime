f944edff2fda49f4cb02c397d4df6e6355a2b29a
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-12-02 16:04:18 -0800

    try using win_setting/setting.ps1 --proxydisable

--- recipes/vc14.rb
+++ recipes/vc14.rb
@@ -18,120 +18,14 @@
 # limitations under the License.
 #
 
-Chef::Resource::RemoteFile.send(:include, Vcruntime::Helper)
 Chef::Resource::PowershellScript.send(:include, Vcruntime::Helper)
-
-def install_dependency
-  hotfix_package_name = ::File.basename(node['KB2999226']['url'])
-  Chef::Log.info "package_url=#{node['KB2999226']['url']}"
-  require 'uri'
-  uri = URI.parse(node['KB2999226']['url'])
-
-  remote_file "#{Chef::Config[:file_cache_path]}\\#{hotfix_package_name}" do
-    source node['KB2999226']['url']
-    checksum node['KB2999226']['checksum']
-    # FIXME: we're using this guard twice Get-Hotfix -ID KB2999226
-    not_if { has_hotfix?('KB2999226') }
-  end
-
-  basename = File.basename(uri.path, '.msu')
-  cabfile="#{basename}.cab"
-  Chef::Log.info "cabfile=#{cabfile}"
-
-  powershell_script 'Install KB2999226' do
-    code <<-EOH
-  # https://goo.gl/xt3Asq
-  mkdir -Force "#{Chef::Config[:file_cache_path]}\\#{basename}"
-  expand -f:* "#{Chef::Config[:file_cache_path]}\\#{hotfix_package_name}" "#{Chef::Config[:file_cache_path]}\\#{basename}"
-  $ErrorActionPreference = 'SilentlyContinue'
-  dism.exe /Online /Add-Package /PackagePath:"#{Chef::Config[:file_cache_path]}\\#{basename}\\#{cabfile}"
-  # Error 775: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681388(v=vs.85).aspx
-  # Error 1151: The specified package is not applicable to this image.
-  if(-not($LastExitCode -eq 775 -or $LastExitCode -eq 1151)){ throw }
-    EOH
-    # FIXME
-    ignore_failure true # fails on mwrock/Windows2012R2
-    # FIXME: we're using this guard twice Get-Hotfix -ID KB2999226
-    not_if { has_hotfix?('KB2999226') }
-  end
-end
+Chef::Resource::RemoteFile.send(:include, Vcruntime::Helper)
 
 include_recipe 'chrome'
 
-# try suggestion from http://answers.microsoft.com/en-us/windows/forum/windows8_1-update/visual-c-2015-redistributable-error/ae9a0c2d-a23b-4dd7-ab6b-cbefb8908c99
-powershell_script 'Disable automatic proxy detection' do
-  code <<-EOH1
-get-service wuauserv | where {$_.status -eq 'running'} | stop-service -force -pass
-set-location "c:\\windows"
-rename-item "c:\\windows\\SoftwareDistribution" "c:\\windows\\SoftwareDistribution.bck"
-start-service wuauserv
-  EOH1
-end
-
-powershell_script 'Disable automatic proxy detection' do
-  ignore_failure true
-  code <<-EOH
-function Disable-AutomaticallyDetectProxySettings
-{
-    Write-Host "FIXME: debug, we got here1"
-
-    # Read connection settings from Internet Explorer.
-    $regKeyPath = "HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections\\"
-    $conSet = $(Get-ItemProperty $regKeyPath).DefaultConnectionSettings
- 
-    Write-Host "FIXME: debug, we got here2"
-
-    # Index into DefaultConnectionSettings where the relevant flag resides.
-    $flagIndex = 8
- 
-    # Bit inside the relevant flag which indicates whether or not to enable automatically detect proxy settings.
-    $autoProxyFlag = 8
- 
-    Write-Host "FIXME: debug, we got here3"
-
-    if ($($conSet[$flagIndex] -band $autoProxyFlag) -eq $autoProxyFlag)
-    {
-
-        Write-Host "FIXME: debug, we got here4"
-
-        # 'Automatically detect proxy settings' was enabled, adding one disables it.
-        Write-Host "Disabling 'Automatically detect proxy settings'."
-        $mask = -bnot $autoProxyFlag
-        $conSet[$flagIndex] = $conSet[$flagIndex] -band $mask
-        $conSet[4]++
-        Set-ItemProperty -Path $regKeyPath -Name DefaultConnectionSettings -Value $conSet
-
-        Write-Host "FIXME: debug, we got here5"
-
-    }
-    
-    $conSet = $(Get-ItemProperty $regKeyPath).DefaultConnectionSettings
-
-    Write-Host "FIXME: debug, we got here6"
-
-    if ($($conSet[$flagIndex] -band $autoProxyFlag) -ne $autoProxyFlag)
-    {
-    	Write-Host "'Automatically detect proxy settings' is disabled."
-    }
-}
-
-Write-Host "FIXME: debug, we got here7"
-
-$job = Start-Job -ScriptBlock { Start-Process "C:\\Program Files\\Internet Explorer\\iexplore.exe" -Wait -PassThru }
-Start-Sleep -Seconds 60
-Stop-Job -Id $job.Id
-
-Write-Host "FIXME: debug, we got here8"
-
-Disable-AutomaticallyDetectProxySettings
-
-Write-Host "FIXME: debug, we got here9"
-
-Set-Itemproperty "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections" -Name ProxyEnable -Value 0
-  EOH
-end
-
 if node.attribute?('KB2999226') and !node['KB2999226']['url'].to_s.empty?
+  clear_software_disribution_folder
+  disable_auto_proxy_detection
   install_dependency
 end
 
